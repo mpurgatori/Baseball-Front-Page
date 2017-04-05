258798f0502e7acb541a67bc0fec5d4f7a7a636b
Mike Purgatori <mpurgatori@gmail.com>
2017-04-05 13:47:42 -0500

    Fetching Data for date and 4 previous dates

--- js/main.js
+++ js/main.js
@@ -1,31 +1,84 @@
 
 
 
-
 const today = new Date()
-const todayFormat = `year_${today.getFullYear()}/month_${("0" + (today.getMonth() + 1)).slice(-2)}/day_${('0'+today.getDate()).slice(-2)}`
 
-let yesterday = new Date(today)
-yesterday.setDate(today.getDate() - 1)
-
-const yesterdayFormat = `year_${yesterday.getFullYear()}/month_${("0" + (yesterday.getMonth() + 1)).slice(-2)}/day_${('0'+yesterday.getDate()).slice(-2)}`
-
-console.log(yesterdayFormat)
-
-const currentDay = ()=> {
-  fetch(`http://gd2.mlb.com/components/game/mlb/${todayFormat}/miniscoreboard.json`, {
-  method: 'get'
-  }).then(response => {
-    return response.json()
-    .then(obj => {
-      let todayScoreBoard = obj.data.games;
-      console.log(todayScoreBoard);
-  })
-    //document.getElementById('vueCommit').innerHTML = gitHubData.vue.lastWeekCommits;
-  }).catch(err => {
-    console.log(err);
-  })
+const callArrayContructor = (date) => {
+  let dateIterate = new Date(date)
+  let callArray = []
+  for (let i=0;i<5;i++){
+    dateIterate.setDate(date.getDate() - i)
+    let dateFormat = `year_${dateIterate.getFullYear()}/month_${("0" + (dateIterate.getMonth() + 1)).slice(-2)}/day_${('0'+dateIterate.getDate()).slice(-2)}`
+    callArray.push(`http://gd2.mlb.com/components/game/mlb/${dateFormat}/miniscoreboard.json`)
+  }
+  return callArray
 }
-//Run currentDay once on load
-currentDay()
-//Run once every minute to retrieve any new data
+
+const requestCreate = array => {
+  let fetchArray = []
+  for (let i=0;i<array.length;i++){
+    fetchArray.push(fetch(array[i], {
+      method: 'get'
+      }).then(response => {
+        return response.json()
+        .then(obj => {
+          return obj.data.games
+      })
+      }).catch(err => {
+        return err
+      })
+    )
+  }
+  return fetchArray
+}
+
+
+Promise.all(requestCreate(callArrayContructor(today)))
+  .then(gameScores => console.log(gameScores))
+
+
+// const findTeam = theData => {
+//     let theGame = {}
+//     for (let i=0;i<theData.game.length;i++){
+//         if (theData.game[i].home_team_name === 'Tigers'|| theData.game[i].away_team_name === 'Tigers'){
+//             theGame = theData.game[i]
+//         }
+//     }
+//     if (theGame === {}){return false}
+//     return theGame
+// }
+
+
+
+// const findPreviousGame = gameData => {
+//     let theGame = {}
+//     for (let i=0;i<gameData.length;i++){
+//         if (gameData[i].status === 'Final'){
+//             theGame = gameData[i]
+//         }
+//     }
+//      if (theGame === {}){return false}
+//     return theGame
+// }
+
+
+
+
+
+// const previousGame = (apiUrl)=> {
+//   fetch(apiUrl, {
+//   method: 'get'
+//   }).then(response => {
+//     return response.json()
+//     .then(obj => {
+//       let scoreBoard = obj.data.games;
+//       findPreviousGame(findTeam(scoreBoard))
+//   })
+//     //document.getElementById('vueCommit').innerHTML = gitHubData.vue.lastWeekCommits;
+//   }).catch(err => {
+//     console.log(err);
+//   })
+// }
+// //Run currentDay once on load
+// currentDay()
+// //Run once every minute to retrieve any new data
