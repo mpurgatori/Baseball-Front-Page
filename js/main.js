71ed0ab39459e467e6802825e377b4f65cb7f256
Mike Purgatori <mpurgatori@gmail.com>
2017-04-05 17:48:31 -0500

    Finding previous game for team

--- js/main.js
+++ js/main.js
@@ -1,13 +1,39 @@
 
+//Helper function to sift through scoreboard object and find game of particular team
+const findTeam = scoreboard => {
+    let theGame = {}
+    for (let i=0;i<scoreboard.game.length;i++){
+        if (scoreboard.game[i].home_team_name === 'Padres'|| scoreboard.game[i].away_team_name === 'Padres'){
+          if (scoreboard.game[i].status === 'Final' || scoreboard.game[i].status === 'Postponed'){
+            theGame = scoreboard.game[i]
+            return theGame
+          }
+        }
+    }
+    return false
+}
+
+//Function that finds previous game
+const findPreviousGame = gameData => {
+    let theGame = {}
+    for (let i=0;i<gameData.length;i++){
+        if (gameData[i].status === 'Final'||gameData[i] === 'Postponed'){
+            theGame = gameData[i]
+        }
+    }
+     if (theGame === {}){return false}
+    return theGame
+}
 
 
-const today = new Date()
 
-const callArrayContructor = (date) => {
-  let dateIterate = new Date(date)
+//Function that creates an array of ten api calls, one for every day from 5 days before today, today and 4 days from now
+const callArrayContructor = () => {
+  let dateIterate = new Date()
+  dateIterate.setDate(dateIterate.getDate() + 5)
   let callArray = []
-  for (let i=0;i<5;i++){
-    dateIterate.setDate(date.getDate() - i)
+  for (let i=0;i<10;i++){
+    dateIterate.setDate(dateIterate.getDate() - 1)
     let dateFormat = `year_${dateIterate.getFullYear()}/month_${("0" + (dateIterate.getMonth() + 1)).slice(-2)}/day_${('0'+dateIterate.getDate()).slice(-2)}`
     callArray.push(`http://gd2.mlb.com/components/game/mlb/${dateFormat}/miniscoreboard.json`)
   }
@@ -20,12 +46,12 @@ const requestCreate = array => {
     fetchArray.push(fetch(array[i], {
       method: 'get'
       }).then(response => {
-        return response.json()
+          return response.json()
         .then(obj => {
           return obj.data.games
       })
       }).catch(err => {
-        return err
+          return err
       })
     )
   }
@@ -33,33 +59,33 @@ const requestCreate = array => {
 }
 
 
-Promise.all(requestCreate(callArrayContructor(today)))
-  .then(gameScores => console.log(gameScores))
+// let requestDateFormat = new Date()
+// requestDateFormat.setDate(requestDateFormat.getDate() + 5)
+// let requestDateFormat = `${requestDateFormat.getFullYear()}${("0" + (requestDateFormat.getMonth() + 1)).slice(-2)}${('0'+requestDateFormat.getDate()).slice(-2)}`
 
 
-// const findTeam = theData => {
-//     let theGame = {}
-//     for (let i=0;i<theData.game.length;i++){
-//         if (theData.game[i].home_team_name === 'Tigers'|| theData.game[i].away_team_name === 'Tigers'){
-//             theGame = theData.game[i]
-//         }
-//     }
-//     if (theGame === {}){return false}
-//     return theGame
-// }
+Promise.all(requestCreate(callArrayContructor()))
+  .then(gameScores => {
+    console.log(gameScores)
+    let previousGame = {}
+    for (let i=4;i<=9;i++){
+      if(findTeam(gameScores[i])) {
+        previousGame = findTeam(gameScores[i])
+        return previousGame
+      }
+    }
+  })
+  .then( gameObj => console.log(gameObj))
 
 
 
-// const findPreviousGame = gameData => {
-//     let theGame = {}
-//     for (let i=0;i<gameData.length;i++){
-//         if (gameData[i].status === 'Final'){
-//             theGame = gameData[i]
-//         }
-//     }
-//      if (theGame === {}){return false}
-//     return theGame
-// }
+
+
+
+
+
+
+
 
 
 
