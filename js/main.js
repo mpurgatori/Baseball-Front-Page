b5fec0ec3f5aeefbe53130ae60d6c78fdff3991a
Mike Purgatori <mpurgatori@gmail.com>
2017-04-18 12:35:41 -0500

    team colors except cleveland

--- js/main.js
+++ js/main.js
@@ -50,12 +50,12 @@ const run = (team) => {
         document.getElementById('prevDate').innerHTML = dateOfGame(previousGame.time_date, previousGame.day)
         document.getElementById('prevAwayCity').innerHTML = `${teamAsset[previousGame.away_team_name].city}`
         document.getElementById('prevAwayTeam').innerHTML = `${previousGame.away_team_name}`
-        document.getElementById('prevAwayRecord').innerHTML = `${previousGame.away_win}-${previousGame.away_loss}`
+        document.getElementById('prevAwayRecord').innerHTML = `(${previousGame.away_win}-${previousGame.away_loss})`
         document.getElementById('atPrev').innerHTML = '@'
         document.getElementById('prevScore').innerHTML = `${previousGame.away_team_runs || null} - ${previousGame.home_team_runs || null}`
         document.getElementById('prevHomeCity').innerHTML = `${teamAsset[previousGame.home_team_name].city}`
         document.getElementById('prevHomeTeam').innerHTML = `${previousGame.home_team_name}`
-        document.getElementById('prevHomeRecord').innerHTML = `${previousGame.home_win}-${previousGame.home_loss}`
+        document.getElementById('prevHomeRecord').innerHTML = `(${previousGame.home_win}-${previousGame.home_loss})`
         document.getElementById('prevStatus').innerHTML = previousGame.status
       }
       return gameScores
@@ -78,15 +78,17 @@ const run = (team) => {
         document.getElementById('nextTime').innerHTML = `${nextGame.home_time} ${nextGame.home_ampm} ${nextGame.home_time_zone}`
         document.getElementById('nextAwayCity').innerHTML = `${teamAsset[nextGame.away_team_name].city}`
         document.getElementById('nextAwayTeam').innerHTML = `${nextGame.away_team_name}`
-        document.getElementById('nextAwayRecord').innerHTML = `${nextGame.away_win}-${nextGame.away_loss}`
+        document.getElementById('nextAwayRecord').innerHTML = `(${nextGame.away_win}-${nextGame.away_loss})`
         document.getElementById('atNext').innerHTML = '@'
         if (nextGame.away_team_runs){document.getElementById('nextScore').innerHTML = `${nextGame.away_team_runs} - ${nextGame.home_team_runs}`}
         document.getElementById('nextHomeCity').innerHTML = `${teamAsset[nextGame.home_team_name].city}`
         document.getElementById('nextHomeTeam').innerHTML = `${nextGame.home_team_name}`
-        document.getElementById('nextHomeRecord').innerHTML = `${nextGame.home_win}-${nextGame.home_loss}`
+        document.getElementById('nextHomeRecord').innerHTML = `(${nextGame.home_win}-${nextGame.home_loss})`
         if (nextGame.inning) { document.getElementById('nextStatus').innerHTML = `${nextGame.status}: Inning ${nextGame.inning}` }
         else { document.getElementById('nextStatus').innerHTML = nextGame.status }
       }
+      $(".full").css("background-color",teamAsset[team].primary)
+       $(".card").css("background-color",teamAsset[team].secondary)
       $("#logo").attr("src",teamAsset[team].logo1);
       $('#gameInfo').fadeIn(400)
       $('#changeTeam').fadeIn(400)
