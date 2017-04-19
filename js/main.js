d8260e3332225359e78e8c0c9bd1bbb36dfe9df4
Mike Purgatori <mpurgatori@gmail.com>
2017-04-19 11:01:37 -0500

    lots of chrome extension updates - icon opens new tab

--- js/main.js
+++ js/main.js
@@ -1,8 +1,12 @@
 
+//Store team information locally 
+const store = localStorage
+ 
+//Check if team info has been stored and load that team. if not set new team info
 const setOrLoadTeam = () => {
   $('#gameInfo').hide()
   $('#changeTeam').hide()
-  let theSelectedTeam = Cookies.get('team') || null
+  let theSelectedTeam = store.getItem('team')|| null
   if (theSelectedTeam) {
     run(theSelectedTeam)
   }
@@ -12,24 +16,26 @@ const setOrLoadTeam = () => {
       e.preventDefault()
       let userTeam = document.getElementById('sel1')
       let selectedTeam = userTeam.options[userTeam.selectedIndex].value
-      Cookies.set('team', selectedTeam)
+      store.setItem('team', selectedTeam)
       theSelectedTeam = selectedTeam
         run(theSelectedTeam)
     })
   }
 }
 
+//Handles change team button
 const changeTeam = () => {
   $('#changeTeam').click((e) => {
     e.preventDefault()
-    Cookies.remove('team')
+    store.removeItem('team')
     $('#gameInfo').hide()
     $('#changeTeam').hide()
+     $(".full").css("background-color","#ffffff")
     setOrLoadTeam()
   })
 }
 
-
+//Retieves game information for selected team and updates screen
 const run = (team) => {
   $('#teamSelector').hide()
   Promise.all(requestCreate(callArrayContructor()))
@@ -41,11 +47,11 @@ const run = (team) => {
           break
         }
       }
-      console.log(previousGame)
       if (isEmpty(previousGame)) { document.getElementById('prev').innerHTML = 'No Previous Game' }
       else {
         $("#prevAwayLogo").attr("src",teamAsset[previousGame.away_team_name].logo2);
         $("#prevHomeLogo").attr("src",teamAsset[previousGame.home_team_name].logo2);
+        $("#prevLink").attr("href", `http://www.mlb.com${previousGame.wrapup_link}`)
         document.getElementById('prev').innerHTML = 'Last Game'
         document.getElementById('prevDate').innerHTML = dateOfGame(previousGame.time_date, previousGame.day)
         document.getElementById('prevAwayCity').innerHTML = `${teamAsset[previousGame.away_team_name].city}`
@@ -72,8 +78,9 @@ const run = (team) => {
       else {
         if (nextGame.status !== 'Preview') { document.getElementById('next').innerHTML = 'Current Game' }
         else { document.getElementById('next').innerHTML = 'Next Game' }
-        $("#nextAwayLogo").attr("src",teamAsset[nextGame.away_team_name].logo2);
-        $("#nextHomeLogo").attr("src",teamAsset[nextGame.home_team_name].logo2);
+        $("#nextAwayLogo").attr("src",teamAsset[nextGame.away_team_name].logo2)
+        $("#nextHomeLogo").attr("src",teamAsset[nextGame.home_team_name].logo2)
+        $("#nextLink").attr("href", `http://www.mlb.com${nextGame.preview_link}`)
         document.getElementById('nextDate').innerHTML = dateOfGame(nextGame.time_date, nextGame.day)
         document.getElementById('nextTime').innerHTML = `${nextGame.home_time} ${nextGame.home_ampm} ${nextGame.home_time_zone}`
         document.getElementById('nextAwayCity').innerHTML = `${teamAsset[nextGame.away_team_name].city}`
@@ -81,6 +88,7 @@ const run = (team) => {
         document.getElementById('nextAwayRecord').innerHTML = `(${nextGame.away_win}-${nextGame.away_loss})`
         document.getElementById('atNext').innerHTML = '@'
         if (nextGame.away_team_runs){document.getElementById('nextScore').innerHTML = `${nextGame.away_team_runs} - ${nextGame.home_team_runs}`}
+        else {document.getElementById('nextScore').innerHTML = null}
         document.getElementById('nextHomeCity').innerHTML = `${teamAsset[nextGame.home_team_name].city}`
         document.getElementById('nextHomeTeam').innerHTML = `${nextGame.home_team_name}`
         document.getElementById('nextHomeRecord').innerHTML = `(${nextGame.home_win}-${nextGame.home_loss})`
@@ -88,7 +96,7 @@ const run = (team) => {
         else { document.getElementById('nextStatus').innerHTML = nextGame.status }
       }
       $(".full").css("background-color",teamAsset[team].primary)
-       $(".card").css("background-color",teamAsset[team].secondary)
+      $(".card").css("background-color",teamAsset[team].secondary)
       $("#logo").attr("src",teamAsset[team].logo1);
       $('#gameInfo').fadeIn(400)
       $('#changeTeam').fadeIn(400)
